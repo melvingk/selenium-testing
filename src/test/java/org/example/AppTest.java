299abb2a40fb31877527141784d253c2a3520918
melvingk <melvin.kamau@academytrainee.com>
2020-04-28 15:31:30 +0100

    Added the webdriver with before and after methods

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,7 +1,25 @@
 package org.example;
 
 
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppTest {
 
+    private WebDriver driver;
+
+@Before
+    public void setup(){
+    driver = new ChromeDriver();
+}
+
+
+
+@After
+    public void teardown(){
+    driver.close();
+}
 
 }
