20a6f02acd152c9df529c592b017153d2520a06b
melvingk <melvin.kamau@academytrainee.com>
2020-04-28 17:35:35 +0100

    Set up the test for register and login

--- src/test/java/org/example/TheDemoSiteExercise.java
+++ src/test/java/org/example/TheDemoSiteExercise.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TheDemoSiteExercise {
@@ -24,6 +25,43 @@ public class TheDemoSiteExercise {
     public void registerUser() throws InterruptedException{
 
         driver.get("http://thedemosite.co.uk/addauser.php");
+        sleep(1000);
+        WebElement usernameRegister = driver.findElement(By.name("username"));
+        WebElement passwordRegister = driver.findElement(By.name("password"));
+        WebElement submitButtonRegister = driver.findElement(By.name("FormsButton2"));
+
+
+        usernameRegister.sendKeys("Tanjiro");
+        passwordRegister.sendKeys("Nezuko");
+        submitButtonRegister.click();
+        sleep(1000);
+
+        driver.get("http://thedemosite.co.uk/login.php");
+        sleep(1000);
+        WebElement usernameLogin = driver.findElement(By.name("username"));
+        WebElement passwordLogin = driver.findElement(By.name("password"));
+        WebElement submitButtonLogin = driver.findElement(By.name("FormsButton2"));
+        usernameLogin.sendKeys("Tanjiro");
+        passwordLogin.sendKeys("Nezuko");
+        sleep(1000);
+        submitButtonLogin.click();
+//        passwordField.sendKeys("Nezuko");
+//        submitButton.click();
+//        sleep(2000);
+//        assertTrue("**Successful Login**",true);
+//        if (confirmRegister.getText().equals(name)){
+//            System.out.println("They match");
+//            assertTrue(true);
+//        }
+//        else
+//            {
+//            System.out.println("It failed");
+//            assertFalse(false);
+//        }
+
+
+
+
 
 
 
