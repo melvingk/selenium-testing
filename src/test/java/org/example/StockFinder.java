9db4e35b96e4b8780d19c046108b8a24806f14a5
melvingk <melvin.kamau@academytrainee.com>
2020-04-30 18:33:48 +0100

    Added a scroller to the location of the risers table; Added screenshot functionality

--- src/test/java/org/example/StockFinder.java
+++ src/test/java/org/example/StockFinder.java
@@ -3,15 +3,14 @@ package org.example;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.maven.surefire.shade.org.apache.maven.shared.utils.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.io.File;
+import java.io.IOException;
 
 import static java.lang.Thread.sleep;
 import static org.testng.Assert.assertEquals;
@@ -42,7 +41,7 @@ public class StockFinder {
         driver = new ChromeDriver();
     }
     @Test
-    public void stockMarketHighestRiser() throws InterruptedException {
+    public void stockMarketHighestRiser() throws InterruptedException, IOException {
         test = report.startTest("Identifying the highest riser");
         driver.manage().window().maximize();
         test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
@@ -64,18 +63,28 @@ public class StockFinder {
         riserTab.click();
         sleep(5000);
         WebElement confirmOnRiserPage = driver.findElement(By.className("main"));
-        test.log(LogStatus.INFO, "Navigating to FTSE 100 riser page");
+        test.log(LogStatus.INFO, "Navigating to FTSE 100 Riser page");
         assertEquals(confirmOnRiserPage.getText(),"FTSE 100: TOP 20 RISERS");
         WebElement theHighestRiser = driver.findElement(By.xpath("//*[@id=\"ls-row-RB.-L\"]/td[2]"));
         test.log(LogStatus.INFO, "The highest risers in the FTSE 100 is " + theHighestRiser.getText());
-
+        if(driver instanceof JavascriptExecutor)
+        {
+            ((JavascriptExecutor) driver).executeScript( "window.scrollBy(0,1000)");
+            sleep(1000);
+        }
+        else{
+            System.out.println("Can't execute JS");
+        }
+        File picture = ((TakesScreenshot) driver). getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(picture, new File(System.getProperty("user.dir") + "/test-output/stockRiserPage.jpg"));
+        test.log(LogStatus.PASS,"The highest riser was seen here ", "<img src=stockRiserPage.jpg>");
 
     }
 
 
 
 
-//        sleep(2000);
+
 
 
 
